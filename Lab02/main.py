6892c8f928c5988a0521209afc59ef9896d5d82a
kkard2 <contact@kkard2.com>
2025-03-14 12:34:18 +0100

    mostly done

--- Lab02/main.py
+++ Lab02/main.py
@@ -3,7 +3,7 @@ from deserialize_json import DeserializeJson
 from serialize_json import SerializeJson
 from convert_json_to_yaml import ConvertJsonToYaml
 
-config = yaml.load(open('Assets/basic_config.yaml', encoding="utf8"),
+config = yaml.load(open('Assets/config.yaml', encoding="utf8"),
                    Loader=yaml.FullLoader)
 
 # TODO: detect malformed config file
@@ -11,14 +11,25 @@ config = yaml.load(open('Assets/basic_config.yaml', encoding="utf8"),
 sd = config['source_directory']
 
 for action in config['actions']:
-    src = sd + action['source_file']
+    t = action['type']
+    src = sd + action['source']
+    dst = None
 
-    if 'json_destination_file' in action:
-        df = DeserializeJson(src)
-        SerializeJson.run(df, sd + action['json_destination_file'])
-    elif 'yaml_destination_file' in action:
+    if 'destination' in action:
+        dst = sd + action['destination']
 
+    print('executing step ' + t + ', src: ' + src + ', dst: ' + str(dst))
 
+    if t == 'json_stats':
+        DeserializeJson(src).somestats()
+    elif t == 'json_select':
+        SerializeJson.run(DeserializeJson(src), dst)
+    elif t == 'json_object_convert_to_yaml':
+        ConvertJsonToYaml.run(DeserializeJson(src), dst)
+    elif t == 'json_path_convert_to_yaml':
+        ConvertJsonToYaml.run2(src, dst)
+    else:
+        print('invalid step type, skipping')
 
 
 """
